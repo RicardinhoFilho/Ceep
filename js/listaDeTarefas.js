ce4b5a7bbcb7c35446a90d9bb92907b2d110412a
Ricardo Filho <ricardinhogiasson16@gmail.com>
2021-02-18 16:54:18 -0300

    Adicionando Botão Concluir e implementando sua funcionalidade/Inserindo todo o código dentro de IIFE

--- js/listaDeTarefas.js
+++ js/listaDeTarefas.js
@@ -1,43 +1,72 @@
 // const teste = document.querySelector(".content")
 // teste.textContent = "Comprar Morango";
 
-const criarTarefa = (evento) =>{
-    
-    evento.preventDefault();
+//estamos criando nosso código dentro de uma "IIFE"(Immediately invoked function expression -> '((){}())' )para que no console no navegador, nosso usuário não tenha acesso ao nosso código/regra de negócio 
 
-    const lista= document.querySelector("[data-list]");//percorrendo até o elemento "ul" de nossa lista
-    const input = document.querySelector("[data-form-input]"); //coletando a entrada do nosso usuário com sua nova tarefa
-    const valor = input.value;//atribuindo à variavél 'valor' o valor do input que o usuário digitou
-    
-    const novaTarefa = document.createElement("li");//criando a variavel nova tarefa que nada mais é do que um novo elemento 'li'
+(() => {
 
-    novaTarefa.classList.add("task");//adicionando classe task em nosso novo elemento li para receber os estilos de nosso css com fundo claro
+    const criarTarefa = (evento) => {
 
-    const conteudo = `<p class="content">${valor}</p>`;//criando constante conteúdo = um paragrafo que recebe a variavél valor que é o valor da entrada de nosso usuário
+        evento.preventDefault();//bloqueia o reload da página quando nós adicionarmos uma nova tarefa 
 
-    novaTarefa.innerHTML=conteudo;//adicionando o html 'conteudo' em nossa 'li'
+        const lista = document.querySelector("[data-list]");//percorrendo até o elemento "ul" de nossa lista
+        const input = document.querySelector("[data-form-input]"); //coletando a entrada do nosso usuário com sua nova tarefa
+        const valor = input.value;//atribuindo à variavél 'valor' o valor do input que o usuário digitou
 
-    lista.appendChild(novaTarefa);//adicionando um novo filho à nossa váriavel "lista" que é nosso elemento ul'lista'
-    
-    input.value = " "; //zerando nosso input
+        const novaTarefa = document.createElement("li");//criando a variavel nova tarefa que nada mais é do que um novo elemento 'li'
 
-    //tarefa.innerHTML = conteudo;
-}
+        novaTarefa.classList.add("task");//adicionando classe task em nosso novo elemento li para receber os estilos de nosso css com fundo claro
 
-const novaTarefa = document.querySelector('[data-form-button]')//Seleciona 'ul'
+        const conteudo = `<p class="content">${valor}</p>`;//criando constante conteúdo = um paragrafo que recebe a variavél valor que é o valor da entrada de nosso usuário
 
+        novaTarefa.innerHTML = conteudo;//adicionando o html 'conteudo' em nossa 'li'
 
-//console.log(input);
+        lista.appendChild(novaTarefa);//adicionando um novo filho à nossa váriavel "lista" que é nosso elemento ul'lista'
 
+        input.value = " "; //zerando nosso input
 
-novaTarefa.addEventListener("click",criarTarefa) //ao clicar no submit a função criar tarefa é acionada
+        novaTarefa.appendChild(BotaoConcluir());//Adicionando um novo filho por meio do método BotãoConcluir()
 
+        //tarefa.innerHTML = conteudo;
+    }
 
+    const novaTarefa = document.querySelector('[data-form-button]')//Seleciona 'ul'
 
 
+    //console.log(input);
+
+
+    novaTarefa.addEventListener("click", criarTarefa) //ao clicar no submit a função criar tarefa é acionada
 
 
 
+
+
+    //Cria o botão concluir 
+    const BotaoConcluir = () => {
+
+        botaoConcluir = document.createElement('button');//cria um elemento button
+
+        botaoConcluir.classList.add('check-button');//adiciona ao novo button a classse check-button - responsável por estiliza-lo
+
+        botaoConcluir.innerHTML = "Concluir";//Inserimos dentro do button o texto Concluir
+
+        botaoConcluir.addEventListener('click', ConcluirTarefa)//Quando ocorrer o evento de click em cima do botão concluir o mpetodo Concluir tarefa é chamado
+
+        return botaoConcluir;//retorna o botão para quem o chamou, neste caso para novaTarefa
+    }
+
+    //Aplica a ação concluir tarefa
+    const ConcluirTarefa = (evento) => {
+
+        const botaoAlvo = evento.target;//seleciona o botão que foi clicado(target)
+
+        const tarefaCompleta = botaoAlvo.parentElement;//nossa tarefa é um paragrafo dentro de um li, "parentElement" significa que estamos selecionando o elemento pai deste paragrafo, no caso o li
+
+        tarefaCompleta.classList.toggle('done')//toggle é o método que utilizamos quando o evento ocorrer, se não estiver adicionada a classe 'done'(responsável por atribuir o estilo na tarefa concluida) estyá classe será adicionada, caso contrário, está classe será removida, consequentemente o estilo de tarefa concluida será removido também
+    }
+
+})();//fim de nossa IIFE
 
 // Existem outros métodos que podemos utilizar para manipular nós:
 
